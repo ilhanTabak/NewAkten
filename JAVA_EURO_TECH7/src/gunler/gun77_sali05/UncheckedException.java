e7ddaa050ec39a5a72deae8160160e002fa083cc
ilhanTabak <herr.ilhantabak@gmail.com>
2023-02-07 18:37:18 +0100

    CiftSayiBulma

---
+++ JAVA_EURO_TECH7/src/gunler/gun77_sali05/UncheckedException.java
@@ -0,0 +1,18 @@
+package gunler.gun77_sali05;
+
+public class UncheckedException {
+
+    public static void main(String[] args) {
+
+
+        //Unchecked exception
+        //ClassCastException
+
+
+        Object obj=new Object();
+
+       // String str=new (String)obj;
+    }
+
+
+}
