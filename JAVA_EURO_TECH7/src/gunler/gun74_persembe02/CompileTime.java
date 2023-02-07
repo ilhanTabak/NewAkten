4d121b8674bb143b983b264b9d06d624fc4241fd
ilhanTabak <herr.ilhantabak@gmail.com>
2023-02-07 17:56:45 +0100

    CompileTimeError

---
+++ JAVA_EURO_TECH7/src/gunler/gun74_persembe02/CompileTime.java
@@ -0,0 +1,27 @@
+package gunler.gun74_persembe02;
+
+public class CompileTime {
+    public static int multiply(int a,int b){
+
+        return a*b;
+
+
+
+    }
+
+    public static int mutiply(int a,int b,int c){
+
+
+
+        return a*b*c;
+
+
+    }
+
+    public static double multiply(double a,double b){
+
+
+
+        return a*b;
+    }
+}
