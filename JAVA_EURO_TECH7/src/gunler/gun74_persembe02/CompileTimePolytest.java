6390e74a33dec6d303239dc5bde77c370d8676a4
ilhanTabak <herr.ilhantabak@gmail.com>
2023-02-07 18:00:53 +0100

    circleAria

---
+++ JAVA_EURO_TECH7/src/gunler/gun74_persembe02/CompileTimePolytest.java
@@ -0,0 +1,13 @@
+package gunler.gun74_persembe02;
+
+public class CompileTimePolytest {
+    public static void main(String[] args) {
+
+
+
+
+        CompileTime.multiply(2,3);
+        CompileTime.multiply(2.3,3.4);
+        CompileTime.multiply(2,2);
+    }
+}
