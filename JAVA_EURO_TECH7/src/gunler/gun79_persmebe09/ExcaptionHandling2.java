a2abbc3439939b99a871e7029910d54de3eb92d1
ilhanTabak <herr.ilhantabak@gmail.com>
2023-02-10 16:18:37 +0100

    ExceptionHandling2.java

--- JAVA_EURO_TECH7/src/gunler/gun79_persmebe09/ExcaptionHandling2.java
+++ JAVA_EURO_TECH7/src/gunler/gun79_persmebe09/ExcaptionHandling2.java
@@ -1,5 +1,7 @@
 package gunler.gun79_persmebe09;
 
+import java.util.Scanner;
+
 public class ExcaptionHandling2 {
 
     public static void main(String[] args) {
@@ -7,17 +9,32 @@ public class ExcaptionHandling2 {
 
         System.out.println("Program beginnt");
 
-        int a=0;
+        int a=0,b;
+        int[]arr=new int[3];
+        Scanner scan=new Scanner(System.in);
+
 
         try{
-            a=2/0;
+            a=2/1;
             System.out.println("in dem try");
-            System.out.println(a);
+            //System.out.println(a);
+            arr[0]=10;
+            b=scan.nextInt();
 
         }catch (ArithmeticException e){
 
-
+            System.out.println("Arithmetic Exception fehler!");
             System.out.println(e.getMessage());
+
+        }catch (ArrayIndexOutOfBoundsException e){
+
+
+            System.out.println("Array bound fehler");
+            System.out.println(e.getMessage());
+        }catch(Exception e){
+
+            System.out.println("gibt es ein Fehler");
+            System.out.println(e.toString());
 
         }
         System.out.println("program beendet");
