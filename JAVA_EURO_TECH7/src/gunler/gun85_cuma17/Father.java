6be7a72f1bbc368a9212c215865b3cb955d1ef09
ilhanTabak <herr.ilhantabak@gmail.com>
2023-03-17 01:28:23 +0100

    abstract

---
+++ JAVA_EURO_TECH7/src/gunler/gun85_cuma17/Father.java
@@ -0,0 +1,10 @@
+package gunler.gun85_cuma17;
+
+public abstract class Father {
+
+    int age;
+    int gewicht;
+
+
+    public abstract void arbeit();
+}
---
+++ JAVA_EURO_TECH7/src/gunler/gun85_cuma17/Ogul.java
@@ -0,0 +1,8 @@
+package gunler.gun85_cuma17;
+
+public class Ogul extends Father {
+
+  public void arbeit(){
+
+    }
+}
